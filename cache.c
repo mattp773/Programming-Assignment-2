ce6bcb4a7fe934a4bd65c1e3e5652a0f7b829d89
Matt <mattp773@gmail.com>
2022-04-14 19:57:46 -0400

    seemingly working cache simulation, only counts hits/misses currently

--- cache.c
+++ cache.c
@@ -12,24 +12,72 @@
 // 4. Hit time (in cycles)
 // 5. Miss time (int cycles)
 
+struct cacheLine {
+    int valid;
+    int tag;
+};
+
 int main(int argc, char ** argv) {
+
+    if(argc != 6) {
+        printf("Usage: ./cache <block size in words> <number of blocks> <associativity> <hit time in cycles> <miss time in cycles>\n");
+        return 1;
+    }
+
     int blockSize = atoi(argv[1]);
     int numBlocks = atoi(argv[2]);
     int assoc = atoi(argv[3]);
     int hitTime = atoi(argv[4]);
     int missTime = atoi(argv[5]);
+    printf("number of blocks: %d\n", numBlocks);
 
-    int offsetBits = ceil(log2(blockSize));
     int indexBits = ceil(log2(numBlocks));
-    int tagBits = 31 - offsetBits - indexBits;
+    int offsetBits = ceil(log2(blockSize));
+    // printf("Offset bits: %d\n", offsetBits);
 
 
     //initialize cache
-    int cache[numBlocks][assoc];
-    //probably more here
+    struct cacheLine cache[numBlocks];
+    for(int i = 0; i < numBlocks; i++) {
+        for(int j = 0; j < assoc; j++) {
+            cache[i].valid = 0;
+            cache[i].tag = 0;
+        }
+    }
 
     //read input file and parse the addresses
-    
+    FILE * fp = fopen("input.txt", "r");
+    if(fp == NULL) {
+        printf("Error opening input file\n");
+        return 1;
+    }
+    int address;
 
+    int numHits = 0;
+    int numMisses = 0;
+    int index;
+    int tag;
+
+    while(fscanf(fp, "%x", &address) > 0) {
+        index = (address / blockSize) % numBlocks;
+        tag = address >> (indexBits + offsetBits);
+
+        if(cache[index].valid == 0) {
+            cache[index].valid = 1;
+            cache[index].tag = tag;
+            numMisses++;
+            printf("Miss: %x, Tag: %d, Index: %d\n", address, tag, index);
+        }
+        else if(cache[index].valid == 1 && cache[index].tag != tag) {
+            numMisses++;
+            cache[index].tag = tag;
+            printf("Miss: %x, Tag: %d, Index: %d\n", address, tag, index);
+        }
+        else if(cache[index].valid == 1 && cache[index].tag == tag) {
+            printf("Hit: %x, Tag: %d, Index: %d\n", address, tag, index);
+            numHits++;
+        }
+    }
+    printf("%d hits and %d misses\n", numHits, numMisses);
     return 0;
 }
