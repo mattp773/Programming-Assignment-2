0582299a2ca9a705a03c9a52647d59dd1643826b
Matt <mattp773@gmail.com>
2022-04-15 20:02:43 -0400

    added some comments and removed unnecessary code

--- cache.c
+++ cache.c
@@ -18,7 +18,7 @@ struct cacheLine {
 };
 
 int main(int argc, char ** argv) {
-    srand(time(NULL));
+    srand(time(NULL)); //seed the randomizer for replacement policy
     //all arguments are required for the program to run
     if(argc != 6) {
         printf("Usage: ./cache <block size in words> <number of blocks> <associativity> <hit time in cycles> <miss time in cycles>\n");
@@ -35,9 +35,8 @@ int main(int argc, char ** argv) {
     //calculate the number of index and offset bits 
     // based on the number of blocks and the block size
     int indexBits = ceil(log2(numBlocks));
-    int offsetBits;
-   
-    offsetBits = ceil(log2(blockSize));
+    int offsetBits = ceil(log2(blockSize));
+
     //initialize cache
     //set all valid bit and tag bits to 0
     struct cacheLine cache[numBlocks/assoc][assoc];
@@ -54,6 +53,7 @@ int main(int argc, char ** argv) {
         printf("Error opening input file\n");
         return 1;
     }
+
     //track the number of addresses read, the number of hits, and the number of misses
     int addressCount = 0;
     int numHits = 0;
@@ -63,10 +63,10 @@ int main(int argc, char ** argv) {
     int address;
     int index;
     int tag;
-    int offset;
     int hit;
     int replaced;
     int randOffset;
+    int i;
     //read in the addresses until the end of file is reached
     while(fscanf(fp, "%x", &address) > 0) {
         hit = 0;
@@ -76,18 +76,15 @@ int main(int argc, char ** argv) {
         //parse the index and tag from the address
         index = ((address >> offsetBits) / blockSize) % (numBlocks/assoc);
         tag = address >> (indexBits + offsetBits);
-        offset = address & 1;
-        printf("Address: %x, Index: %x, Tag: %x, Offset: %x\n", address, index, tag, offset);
 
         //check if the index has valid data
         //if not, increment the number of misses and set the tag and valid bit
         //if it does, check if the tag matches the tag in the cache
         //if the tag does not match, increment the number of misses and set the new tag
         //if the tag does match, increment the number of hits
-        for(int i = 0; i < assoc; i++) {
+        for(i = 0; i < assoc; i++) {
             if(cache[index][i].valid == 1 && cache[index][i].tag == tag) {
                 hit = 1;
-                printf("Hit: %x\n", address);
             }
         }
         if(hit) {
@@ -95,23 +92,21 @@ int main(int argc, char ** argv) {
         }
         else {
             numMisses++;
-            for(int i = 0; i < assoc; i++) {
+            for(i = 0; i < assoc; i++) {
                 if(cache[index][i].valid == 0) {
-                    printf("invalid cache line %d\n", i);
                     cache[index][i].valid = 1;
                     cache[index][i].tag = tag;
                     replaced = 1;
                     break;
                 }
             }
+            //random replacement policy
             if(replaced == 0) {
                 randOffset = rand() % assoc;
-                printf("random offset: %d\n", randOffset);
                 cache[index][randOffset].valid = 1;
                 cache[index][randOffset].tag = tag;
             }
         }
-        
     }
 
     //print the number of hits and misses
